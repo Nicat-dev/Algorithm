624212ef29cef16f92083cccb0d6648119f419ea
Nicat-dev <77841156+Nicat-dev@users.noreply.github.com>
2021-07-24 12:02:45 +0400

    Initial commit

---
+++ src/Question/randomNumber/RandomNumber.java
@@ -0,0 +1,19 @@
+package Question.randomNumber;
+
+//0 ile 1000 arasında random bir say yaradaraq
+// bu sayın reqemleri cemini veren java algoritma kodunu yazın
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random()*(1000));
+        int birlik = randomNumber % 10;
+
+        randomNumber = randomNumber/10;
+
+        int onluq = randomNumber%10;
+
+        int yuzluk = randomNumber/10;
+
+        System.out.println("Toplam : "+(birlik+onluq+yuzluk));
+    }
+}
