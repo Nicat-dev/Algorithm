a7242b0f5444ea900ad1d4a2ffa1c5b11c9674c1
Nicat-dev <77841156+Nicat-dev@users.noreply.github.com>
2021-08-01 15:12:36 +0400

    Initial commit

--- src/Question/camelCase/CamelCase.java
+++ src/Question/camelCase/CamelCase.java
@@ -38,17 +38,8 @@ class Result {
 
 class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String s = bufferedReader.readLine();
-
-        int result = Result.camelcase(s);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        System.out.println(Result.camelcase(word));
     }
 }
