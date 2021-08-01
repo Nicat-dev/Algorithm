9b77826bb9c656d514ab56ff59f1d7be386e2616
Nicat-dev <77841156+Nicat-dev@users.noreply.github.com>
2021-08-01 15:14:28 +0400

    Initial commit

--- src/Question/camelCase/CamelCase.java
+++ src/Question/camelCase/CamelCase.java
@@ -1,26 +1,10 @@
 package Question.camelCase;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'camelcase' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts STRING s as parameter.
-     */
 
     public static int camelcase(String s) {
         // Write your code here
